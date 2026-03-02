4464e654d1e180154e6f4b6c36449145a8b7a4e0
Zer00ne <winnerfahsai@gmail.com>
2026-03-02 19:17:09 +0700

    fix p01

--- src/mission/mission.controller.ts
+++ src/mission/mission.controller.ts
@@ -1,13 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
-import { MissionService } from './mission.service';
+import { AppService } from './app.service';
 
-@Controller('missions')
-export class MissionController {
-  constructor(private readonly missionService: MissionService) {}
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
 
-  @Get('summary') 
-  getSummary() { 
-   
-    return this.missionService.getSummary();
+  @Get()
+  getLove(): string {
+    return this.appService.getLove();
   }
 }
--- src/mission/mission.service.ts
+++ src/mission/mission.service.ts
@@ -11,20 +11,25 @@ export class MissionService {
     { id: 6, codename: 'GHOST_RIDER', status: 'COMPLETED' },
   ];
 
-   getSummary(): { ACTIVE: number; COMPLETED: number; FAILED: number } {
-    const activeCount = this.missions.filter(
-      (m) => m.status === 'ACTIVE',
-    ).length;
-    const completeCount = this.missions.filter(
-      (m) => m.status === 'COMPLETED',
-    ).length;
-    const failedCount = this.missions.filter(
-      (m) => m.status === 'FAILED',
-    ).length;
+  getSummary() {
+    let active = 0;
+    let completed = 0;
+    let failed = 0;
+
+    for (let i = 0; i < this.missions.length; i++) {
+      if (this.missions[i].status === 'ACTIVE') {
+        active = active + 1;
+      } else if (this.missions[i].status === 'COMPLETED') {
+        completed = completed + 1;
+      } else if (this.missions[i].status === 'FAILED') {
+        failed = failed + 1;
+      }
+    }
+
     return {
-      ACTIVE: activeCount,
-      COMPLETED: completeCount,
-      FAILED: failedCount,
+      ACTIVE: active,
+      COMPLETED: completed,
+      FAILED: failed,
     };
   }
 }
